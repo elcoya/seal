3ac6dd6f03a29aaac300b30474579e2c8d124834
El Coya <aniballovaglio@yahoo.com.ar>
2012-10-07 23:16:52 -0300

    configuro un perfil de test para la base de datos

--- seal/settings.py
+++ seal/settings.py
@@ -17,6 +17,14 @@ DATABASES = {
         'PASSWORD': '$3alSEAL',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'seal_test',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
