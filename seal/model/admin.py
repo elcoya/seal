4ef5b6bb98803ffb76b440a42141c76b288d2a9e
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-08 18:10:45 -0300

    agregado de student con foreingKey a course

--- seal/model/admin.py
+++ seal/model/admin.py
@@ -4,6 +4,8 @@ Created on 07/10/2012
 @author: anibal
 '''
 from seal.model.models import Course
+from seal.model.models import Student
 from django.contrib import admin
 
 admin.site.register(Course)
+admin.site.register(Student)
--- seal/model/models.py
+++ seal/model/models.py
@@ -4,4 +4,12 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length = 32)
+    def __str__(self):
+        return self.name
     
+class Student(models.Model):
+    course = models.ForeignKey(Course)
+    name = models.CharField(max_length = 100)
+    uid = models.IntegerField()
+    def __str__(self):
+        return self.name
