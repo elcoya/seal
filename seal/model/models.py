2138fadd1b413687d4f9f06afc4ba040d5134bd1
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-08 19:09:45 -0300

    agregado de unique para name de course y para uid de student

--- seal/model/models.py
+++ seal/model/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Course(models.Model):
-    name = models.CharField(max_length = 32)
+    name = models.CharField(max_length = 32, unique=True)
     def __str__(self):
         return self.name
     
 class Student(models.Model):
     course = models.ForeignKey(Course)
     name = models.CharField(max_length = 100)
-    uid = models.IntegerField()
+    uid = models.IntegerField(unique=True)
     def __str__(self):
         return self.name
