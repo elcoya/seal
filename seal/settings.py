0b1aa8c893a1c4359bac0bd324af341248e33ea3
El Coya <aniballovaglio@yahoo.com.ar>
2012-10-07 23:34:21 -0300

    remuevo el perfil default para que tome el de test directamente para el acceso a la base de datos

--- seal/settings.py
+++ seal/settings.py
@@ -10,14 +10,14 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'seal',                      # Or path to database file if using sqlite3.
-        'USER': 'seal',                      # Not used with sqlite3.
-        'PASSWORD': '$3alSEAL',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
-    },
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'seal',                      # Or path to database file if using sqlite3.
+#        'USER': 'seal',                      # Not used with sqlite3.
+#        'PASSWORD': '$3alSEAL',                  # Not used with sqlite3.
+#        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+#    },
     'test': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'seal_test',                      # Or path to database file if using sqlite3.
