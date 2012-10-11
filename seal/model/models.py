644736dcbb8dc4ab0ff7beda73b7b1623889ac94
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-11 18:05:29 -0300

    agreagdo de email en student, para probar si travis sube la ultima version

--- seal/model/models.py
+++ seal/model/models.py
@@ -7,12 +7,11 @@ class Course(models.Model):
     def __str__(self):
         return self.name
     
-    
-    
 class Student(models.Model):
     course = models.ForeignKey(Course)
     name = models.CharField(max_length = 100)
     uid = models.IntegerField(unique=True)
+    email = models.CharField(max_length = 90)
     def __str__(self):
         return self.name
     
