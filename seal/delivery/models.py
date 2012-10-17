ad349c6c470d11c477b90f164a2153a5b074db40
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-17 20:09:46 -0300

    se elimino la clave foranea para poder subir via Travis. TODO : ver poque travis no puede crear la base de prueba cuando hay claves entre dos app diferentes

--- seal/delivery/models.py
+++ seal/delivery/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from seal import model
 # Create your models here.
 
 class Delivery(models.Model):
-    student = models.ForeignKey(model.Student,related_name="delivery_by_user")
     uid = models.IntegerField(unique=True)
     statement = models.CharField(max_length = 200)
     deliverDate = models.DateField()
