b6290777b0c7ed5880f56818ffeae7bab425943c
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-17 19:29:08 -0300

    agregado de related_name para ver si Travis puede realizar el sync de la base

--- seal/delivery/models.py
+++ seal/delivery/models.py
@@ -3,7 +3,7 @@ from seal.model.student import Student
 # Create your models here.
 
 class Delivery(models.Model):
-    student = models.ForeignKey(Student)
+    student = models.ForeignKey(Student,related_name="delivery_by_user")
     uid = models.IntegerField(unique=True)
     statement = models.CharField(max_length = 200)
     deliverDate = models.DateField()
