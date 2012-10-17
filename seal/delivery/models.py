f65e1f0f812c3e52204c710d65c61da8566a9d12
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-17 19:51:59 -0300

    cambio en la clave foranea para que la encuentre Travis

--- seal/delivery/models.py
+++ seal/delivery/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from seal.model import Student
+from seal import model
 # Create your models here.
 
 class Delivery(models.Model):
-    student = models.ForeignKey(Student,related_name="delivery_by_user")
+    student = models.ForeignKey(model.Student,related_name="delivery_by_user")
     uid = models.IntegerField(unique=True)
     statement = models.CharField(max_length = 200)
     deliverDate = models.DateField()
