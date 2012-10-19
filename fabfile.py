0d1ae9fe78a5dd9e9ba2bc115e1dc60b1a071654
El Coya <aniballovaglio@yahoo.com.ar>
2012-10-19 00:53:54 -0300

    probando fabric

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+'''
+Created on 19/10/2012
+
+@author: anibal
+'''
+from fabric.api import local
+
+def prepare_db():
+    local("mysql -e 'create database seal;'")
+    local("mysql -u root < ci_scripts/ci_dbuser.sql")
+
+def run_tests():
+    local("seal/manage.py run_tests")
+
+def prepare_deploy():
+    prepare_db()
+    run_tests()
+
+def invoke_test_deploy():
+    local("wget http://ixion-tech.com.ar/seal/requestUpdate.php")
+
+def run():
+    prepare_deploy()
+    invoke_test_deploy()
