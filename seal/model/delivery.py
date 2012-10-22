b5c5be2efc92a5b3863a0fba1a3ee530c4656329
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-22 19:24:07 -0300

    cambio en las clases delivery y practice

--- seal/model/delivery.py
+++ seal/model/delivery.py
@@ -4,7 +4,6 @@ from seal.model.student import Student
 class Delivery(models.Model):
     student = models.ForeignKey(Student)
     uid = models.IntegerField(unique=True)
-    statement = models.CharField(max_length = 200)
     deliverDate = models.DateField()
     def __str__(self):
         return self.uid
--- seal/model/practice.py
+++ seal/model/practice.py
@@ -4,7 +4,7 @@ from seal.model.course import Course
 class Practice(models.Model):
     uid = models.IntegerField(unique=True)
     course = models.ForeignKey(Course)
-    statement = models.CharField(max_length = 200)
+    statement = models.TextField()
     deadline = models.DateField()
     def __str__(self):
         return (str(self.uid)+"/"+self.course.name)
