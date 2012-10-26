51b3450f8c04e531e1f30b4599815b705e6055c6
tanomartin05@gmail.com <martin@martin.(none)>
2012-10-26 19:55:04 -0300

    agregado de test unitarios. VER: cuando se corre desde aptana studio 3 no crea base de test, cuando se corre de consola si.

---
+++ seal/test/practiceTest.py
@@ -0,0 +1,34 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+from seal.model import Practice, Course
+
+class PracticeTest(TestCase):
+    
+    def setUp(self):
+        nameCourse = '2012-2'
+        course = Course()
+        course.name = nameCourse
+        course.save()
+        
+    def testPracticeCreation(self):
+        pCourse = Course.objects.get(name='2012-2')
+        pPractice = Practice()
+        pPractice.uid = 1
+        pPractice.course = pCourse
+        pPractice.file = "pathFile"
+        pPractice.deadline = "2012-12-01" 
+        pPractice.save()
+        cPractice = Practice.objects.get(uid=1)
+        self.assertEqual(pPractice.course.name, cPractice.course.name)
+    
+    def tearDown(self):
+        cPractice = Practice.objects.get(uid=1)
+        cPractice.delete()
+        pCourse = Course.objects.get(name='2012-2')
+        pCourse.delete()
--- seal/test/studentTest.py
+++ seal/test/studentTest.py
@@ -6,10 +6,48 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from seal.model import Student,Course
 
-class CourseTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class StudentTest(TestCase):
+    
+    def setUp(self):
+        nameCourse = '2012-1'
+        course = Course()
+        course.name = nameCourse
+        course.save()
+                
+    def testStudentCreation(self):
+        sCourse = Course.objects.get(name='2012-1')
+        pStudent = Student()
+        pStudent.name = "Nombre y Apellido"
+        pStudent.uid = 85000
+        pStudent.email = "email@pagnia.com.ar"
+        pStudent.course = sCourse
+        pStudent.save()
+        cStudent = Student.objects.get(uid=85000)
+        self.assertEqual(pStudent.uid, cStudent.uid)
+        
+    def testStudenteEqualUid(self):
+        sCourse = Course.objects.get(name='2012-1')
+        
+        pStudent = Student()
+        p1Student = Student()
+        
+        pStudent.name = "Nombre y Apellido"
+        pStudent.uid = 50000
+        pStudent.email = "email@pagnia.com.ar"
+        pStudent.course = sCourse
+        #save de first estudent...
+        pStudent.save()
+        
+        p1Student.name = "Nombre y Apellido"
+        p1Student.uid = 85000
+        p1Student.email = "email@pagnia.com.ar"
+        p1Student.course = sCourse
+        
+        try:
+            #try save second student withd equal uid 
+            p1Student.save()
+            assert(False)
+        except Exception:
+            self.assert_("exceptions.BaseException", Exception.message)
