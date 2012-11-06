7aa1d13dd7d8d867d4773e188af12b4c65699e3f
tanomartin05@gmail.com <martin@martin.(none)>
2012-11-06 20:53:49 -0300

    Test unitario de creacion de entrega

--- seal/test/deliveryTest.py
+++ seal/test/deliveryTest.py
@@ -6,11 +6,48 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from seal.model import Course, Delivery, Student, Practice
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        nameCourse = '2012-2'
+        course = Course()
+        course.name = nameCourse
+        course.save()
+        
+        student = Student()
+        student.name = "Nombre y Apellido"
+        student.uid = "85000"
+        student.email = "email@pagnia.com.ar"
+        student.save()
+        
+        practice = Practice()
+        practice.uid = "Tp inicial"
+        practice.course = course
+        practice.file = "pathFile"
+        practice.deadline = "2012-12-01" 
+        practice.save()
+        
+        
+    def testPracticeCreation(self):
+        student = Student.objects.get(uid="85000")
+        practice = Practice.objects.get(uid="Tp inicial")
+        
+        pDelivery = Delivery()
+        pDelivery.file = "pathFile"
+        pDelivery.student = student
+        pDelivery.practice = practice
+        pDelivery.save()
+        
+        cDelivery = pDelivery.objects.get(uid=1)
+        self.assertEqual(pDelivery.pk, cDelivery.pk)
+    
+    def tearDown(self):
+        delivery = Delivery.objects.get(uid=1)
+        delivery.delete()
+        student = Student.objects.get(uid="85000")
+        student.delete()
+        practice = Practice.objects.get(uid="Tp inicial")
+        practice.delete()
+        pCourse = Course.objects.get(name='2012-2')
+        pCourse.delete()
