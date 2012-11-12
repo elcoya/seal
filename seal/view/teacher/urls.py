92575d21789011e158beb03c3ee29ee662563249
tanomartin05@gmail.com <martin@martin.(none)>
2012-11-12 15:48:16 -0300

    Refactor: Cambio de lugar algunas view. Arreglo del checkall

--- seal/view/teacher/urls.py
+++ seal/view/teacher/urls.py
@@ -4,21 +4,21 @@ urlpatterns = patterns('view.teacher.home',
     (r'^$', 'index'),
 )
 
-urlpatterns += patterns('view.practice',
+urlpatterns += patterns('view.teacher.practice',
     url(r'^practices/?$', 'index'),
     url(r'^practices/newpractice/(?P<idcourse>\d+)$', 'newpractice'),
     url(r'^practices/editpractice/(?P<idcourse>\d+)/(?P<idpractice>\d+)/$', 'editpractice'),
     url(r'^practices/download/(?P<idpractice>\d+)/$', 'download')
 )
 
-urlpatterns += patterns('view.student',
+urlpatterns += patterns('view.teacher.student',
     url(r'^students/?$', 'index'),
     url(r'^students/newstudent/(?P<idcourse>\d+)$', 'newstudent'),
     url(r'^students/editstudent/(?P<idcourse>\d+)/(?P<idstudent>\d+)/$', 'editstudent'),
     url(r'^students/editstudent/(?P<idstudent>\d+)/$', 'edit_unenrolled_student'),
 )
 
-urlpatterns += patterns('view.course',
+urlpatterns += patterns('view.teacher.course',
     url(r'^course/?$', 'index'),
     url(r'^course/newcourse/?$', 'newcourse'),
     url(r'^course/editcourse/(?P<idcourse>\d+)$', 'editcourse'),
