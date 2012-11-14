bf4d242c8cd78c4b60c274db3c0d6adfec32b41d
tanomartin05@gmail.com <martin@martin.(none)>
2012-11-14 17:03:41 -0300

    Sacamos el PROJECT_PATH, estaba definido dos veces.

--- seal/settings.py
+++ seal/settings.py
@@ -110,7 +110,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'seal.urls'
 
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
