f9c9d7320a0f8cd5532ece72ed15bf974e8d169d
tanomartin05@gmail.com <martin@martin.(none)>
2013-02-09 16:46:31 -0300

    Eliminacion de mail cuando se envia.

--- daemon/auto_correction/utils/administrator_mail.py
+++ daemon/auto_correction/utils/administrator_mail.py
@@ -47,6 +47,12 @@ class AdministratorMail(object):
         session.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
         return(session)         
 
+    def delete_mail(self, mail):
+        url_to_delete = HTTP_SERIALIZER + str(mail['id'])
+        self.log.debug("Sending delete request for mail id %d", mail['id'])
+        response = requests.delete(url_to_delete, auth=(SERIALIZER_AUTH_USER, SERIALIZER_AUTH_PASS))
+        self.log.info("Delete response: %s", str(response))
+       
     def send_mails(self):     
         mails = self.request_pending_mails()
         if (len(mails) == 0):
@@ -60,3 +66,6 @@ class AdministratorMail(object):
                 session.sendmail(EMAIL_HOST_USER, recipient, mail_prepared)
                 session.quit()
                 self.log.info("Mail sent to " + recipient + " subject " + subject)
+                self.delete_mail(mail)
+                
+                
--- web/seal/view/serializer.py
+++ web/seal/view/serializer.py
@@ -4,28 +4,33 @@ Created on 07/02/2013
 @author: martin
 '''
 
-from rest_framework import generics
+from rest_framework import generics, reverse
 from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from seal.serializers import MailSerializer
 from seal.model.mail import Mail
 
-
 @api_view(['GET'])
-def seal_root(request, format=None):
+def api_root(request, format=None):
     """
     The entry endpoint of our API.
     """
     return Response({
         'mails': reverse('mail-list', request=request),
+        'mails details': reverse('mail-detail', request=request),
     })
 
 class MailList(generics.ListCreateAPIView):
     """
-    SEAL endpoint that represents a list of mail.
+    API endpoint that represents a list of mail.
     """
-    #model = Mail 
-    
-    queryset = Mail.objects.filter(status = 0)
+    queryset = Mail.objects.all()
     serializer_class = MailSerializer
+
+class MailDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that represents a single user.
+    """
+    model = Mail
+    serializer_class = MailSerializer
+
